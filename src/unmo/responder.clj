481f04e6b679f22619d8b2db3c31a6eb6b7bc2ba
sandmark <sandmark.m@gmail.com>
2018-12-13 21:35:46 +0900

    Add responder `what`

--- src/unmo/responder.clj
+++ src/unmo/responder.clj
@@ -2,6 +2,9 @@
 
 (defmulti response :responder)
 
+(defmethod response :what [params]
+  (str (:input params) "ってなに？"))
+
 (defmethod response :default [params]
   (throw (IllegalArgumentException.
           (str "Responder " (params :responder) " が存在しません。"))))
--- test/unmo/responder_test.clj
+++ test/unmo/responder_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [unmo.responder :refer :all]))
 
+(deftest what-responder-test
+  (testing "WhatResponderは"
+    (testing "入力 s に対し、常に \"sってなに？\" と返す"
+      (is (= "テストってなに？" (response {:responder :what :input "テスト"}))))))
+
 (deftest default-responder-test
   (testing "Responderを指定しなかった場合、"
     (testing "例外IllegalArgumentExceptionを投げる"
