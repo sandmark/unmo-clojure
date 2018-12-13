dd3e6333de3c2f919535c72bdcd1a2433eebffb3
sandmark <sandmark.m@gmail.com>
2018-12-13 20:00:22 +0900

    Install eftest

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0-RC5"]
                  [environ "1.1.0"]]
-  :plugins [[lein-environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]
+            [lein-eftest "0.5.3"]]
   :main ^:skip-aot unmo.core
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[alembic "0.3.2"]]}
+  :profiles {:dev {:dependencies [[alembic "0.3.2"]
+                                  [eftest "0.5.3"]]}
              :uberjar {:aot :all}})
