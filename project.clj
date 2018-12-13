c68b97182abe5d3d8498380c36c9fdbf19e76a89
sandmark <sandmark.m@gmail.com>
2018-12-13 20:13:25 +0900

    Install lein-auto

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0-RC5"]
                  [environ "1.1.0"]]
   :plugins [[lein-environ "1.1.0"]
-            [lein-eftest "0.5.3"]]
+            [lein-eftest "0.5.3"]
+            [lein-auto "0.1.3"]]
   :main ^:skip-aot unmo.core
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[alembic "0.3.2"]
