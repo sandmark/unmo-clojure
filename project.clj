43e1eff8694d9240c09e0d7e48d3793084b0fba1
sandmark <sandmark.m@gmail.com>
2018-12-13 19:55:01 +0900

    Install environ

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/sandmark/unmo-clojure/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-RC5"]]
+  :dependencies [[org.clojure/clojure "1.10.0-RC5"]
+                 [environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]]
   :main ^:skip-aot unmo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
