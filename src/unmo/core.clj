68c05b6f2cffde13bd531ad528995d372f48b10f
sandmark <sandmark.m@gmail.com>
2018-12-14 21:49:07 +0900

    Add main-loop

--- src/unmo/core.clj
+++ src/unmo/core.clj
@@ -1,5 +1,8 @@
 (ns unmo.core
-  (:gen-class))
+  (:gen-class)
+  (:require [unmo.responder :refer [response]]
+            [environ.core :refer [env]]))
+
 (defn- format-response
   "Responder からの結果を整形して返す。"
   [{:keys [responder response]}]
@@ -7,6 +10,16 @@
     (str responder-name "> " response)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "標準入力からユーザーの発言を受け取り、Responder の結果を表示して繰り返す。"
   [& args]
-  (println "Hello, World!"))
+  (println (format "Unmo version %s launched." (:unmo-version env)))
+  (print "> ")
+  (flush)
+
+  (loop [input (read-line)]
+    (if (clojure.string/blank? input)
+      (println "Quit.")
+      (do (-> {:responder :what :input input} (response) (format-response) (println))
+          (print "> ")
+          (flush)
+          (recur (read-line))))))
