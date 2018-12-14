396b9cbbfc82fad02e2a121a9e0212090cdff54b
sandmark <sandmark.m@gmail.com>
2018-12-14 21:22:24 +0900

    Add method `format-response`

--- src/unmo/core.clj
+++ src/unmo/core.clj
@@ -1,5 +1,10 @@
 (ns unmo.core
   (:gen-class))
+(defn- format-response
+  "Responder からの結果を整形して返す。"
+  [{:keys [responder response]}]
+  (let [responder-name (-> responder (name) (clojure.string/capitalize))]
+    (str responder-name "> " response)))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/unmo/core_test.clj
+++ test/unmo/core_test.clj
@@ -2,3 +2,9 @@
   (:require [clojure.test :refer :all]
             [unmo.core :refer :all]))
 
+(deftest format-response-test
+  (testing "format-responseは"
+    (let [format-response #'unmo.core/format-response]
+      (testing "Responder からの結果を整形して返す"
+        (are [res] (= "What> テスト" (format-response res))
+          {:responder :what :response "テスト"})))))
