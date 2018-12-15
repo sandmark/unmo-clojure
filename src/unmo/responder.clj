777adb00cfd0cd01c5d5f1dc641c1e239d650ac3
sandmark <sandmark.m@gmail.com>
2018-12-15 12:00:17 +0900

    Add doc-string

--- src/unmo/responder.clj
+++ src/unmo/responder.clj
@@ -1,11 +1,17 @@
 (ns unmo.responder)
 
-(defmulti response :responder)
+(defmulti response
+  "渡された発言オブジェクトに対する返答を :response キーに設定して返す。どの思考エンジンが使用されるかは :responder キーの値で変わる。"
+  :responder)
 
-(defmethod response :what [{:keys [input] :as params}]
+(defmethod
+  ^{:doc "WhatResponderは入力 :input に対し、常に 'inputってなに？' と返す。"}
+  response :what [{:keys [input] :as params}]
   (->> (str input "ってなに？")
        (assoc params :response)))
 
-(defmethod response :default [{:keys [responder]}]
+(defmethod
+  ^{:doc "Responderの指定がない、もしくは存在しないResponderを指定された場合、IllegalArgumentException例外を投げる。"}
+  response :default [{:keys [responder]}]
   (throw (IllegalArgumentException.
           (str "Responder " responder " が存在しません。"))))
