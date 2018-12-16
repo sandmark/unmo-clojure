deb3114a6192fbbc86d76b2fbf2d92c13eed89ab
sandmark <sandmark.m@gmail.com>
2018-12-17 00:23:09 +0900

    Add save/load dictionary

--- src/unmo/dictionary.clj
+++ src/unmo/dictionary.clj
@@ -10,3 +10,16 @@
   "文字列inputを辞書dictionaryに保存したものを返す。"
   [dictionary input]
   (study-random dictionary input))
+
+(defn save-dictionary
+  "辞書dictionaryをpprintし、指定されたファイルに保存する。"
+  [dictionary filename]
+  (let [data (with-out-str (fipp dictionary))]
+    (spit filename data :encoding "UTF-8")))
+
+(defn load-dictionary
+  "指定されたファイルから辞書をロードして返す。"
+  [filename]
+  (if (file-exists? filename)
+    (-> filename (slurp :encoding "UTF-8") (read-string))
+    {}))
--- test/unmo/dictionary_test.clj
+++ test/unmo/dictionary_test.clj
@@ -12,3 +12,8 @@
       (testing "重複した発言は学習しない"
         (is (= {:random ["test"]}
                (study-random {:random ["test"]} "test")))))))
+
+(deftest load-dictionary-test
+  (testing "loadは"
+    (testing "ファイルが存在しない場合、空の辞書を返す"
+      (is (= {} (load-dictionary "dummy.txt"))))))
