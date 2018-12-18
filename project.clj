8a92a312a1a1006dd8c8dcad98d5c7412a35c609
sandmark <sandmark.m@gmail.com>
2018-12-18 23:24:09 +0900

    Upgrade clojure from 1.10.0-RC5 to 1.10.0 (stable release)

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/sandmark/unmo-clojure/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0-RC5"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [environ "1.1.0"]
                  [bigml/sampling "3.2"]
                  [fipp "0.6.14"]]
