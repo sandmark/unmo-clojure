7722c15e70364bea15c2c49dbed16504c6d36d14
sandmark <sandmark.m@gmail.com>
2018-12-21 20:44:17 +0900

    Install Sudachi

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "https://github.com/sandmark/unmo-clojure/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["Sonatype" "https://oss.sonatype.org/content/repositories/snapshots"]]
   :dependencies [[org.clojure/clojure "1.10.0-RC5"]
                  [environ "1.1.0"]
                  [bigml/sampling "3.2"]
-                 [fipp "0.6.14"]]
+                 [fipp "0.6.14"]
+                 [com.worksap.nlp/sudachi "0.1.1-SNAPSHOT"]]
   :plugins [[lein-environ "1.1.0"]
             [lein-eftest "0.5.3"]
             [lein-auto "0.1.3"]]
