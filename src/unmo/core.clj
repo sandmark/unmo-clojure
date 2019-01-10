7c43066f3717bae8649ce58f01690608bc0b8a2d
sandmark <sandmark.m@gmail.com>
2019-01-11 00:58:30 +0900

    Add template-responder to main loop

--- src/unmo/core.clj
+++ src/unmo/core.clj
@@ -13,12 +13,16 @@
 
 (defn- rand-responder
   "確率によって変動するResponderを返す。
-  :what    10%
-  :random  30%
-  :pattern 60%"
+  :what     10%
+  :random   30%
+  :pattern  40%
+  :template 20%"
   []
-  (-> [:what :random :pattern]
-      (simple/sample :weigh {:what 0.1 :random 0.3 :pattern 0.6})
+  (-> [:what :random :pattern :template]
+      (simple/sample :weigh {:what     0.1
+                             :random   0.3
+                             :pattern  0.4
+                             :template 0.2})
       (first)))
 
 (defn- format-response
