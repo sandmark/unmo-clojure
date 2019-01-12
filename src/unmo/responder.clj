1283268caa211b6df5da0a91793ae819efb3061d
sandmark <sandmark.m@gmail.com>
2019-01-12 15:16:37 +0900

    Add markov-responder
    with a support function `markov-generate`

--- src/unmo/responder.clj
+++ src/unmo/responder.clj
@@ -5,6 +5,31 @@
   "渡された発言オブジェクトに対する返答を :response キーに設定して返す。どの思考エンジンが使用されるかは :responder キーの値で変わる。"
   :responder)
 
+(defn- markov-generate
+  "単語と辞書を受け取り、その単語から始まる文章をマルコフ連鎖で生成して返す。"
+  ([dictionary prefix1 prefix2] (markov-generate 30 dictionary prefix1 prefix2 [prefix1 prefix2]))
+  ([times dictionary prefix1 prefix2 result]
+   (let [suffix (-> dictionary (get-in [prefix1 prefix2]) (rand-nth))]
+     (cond (zero? times)          (apply str result)
+           (= "%ENDMARK%" suffix) (apply str result)
+           :else (->> (conj result suffix)
+                      (recur (dec times) dictionary prefix2 suffix))))))
+
+(defmethod
+  ^{:doc "MarkovResponderは形態素解析結果partsを受け取り、最初の単語またはランダムな単語から始まる文章を生成して返す。"}
+  response :markov [{:keys [parts dictionary] :as params}]
+  (if (empty? (:markov dictionary))
+    (assoc params :error {:type :dictionary-empty
+                          :message "マルコフ辞書が空です"})
+    (let [starts (get-in dictionary [:markov :starts])
+          markov (get-in dictionary [:markov :dictionary])
+          word   (ffirst parts)
+          prefix1 (if (contains? starts word)
+                    word
+                    (-> starts (keys) (rand-nth)))
+          prefix2 (-> (get markov prefix1) (keys) (rand-nth))]
+      (assoc params :response (markov-generate markov prefix1 prefix2)))))
+
 (defmethod
   ^{:doc "TemplateResponderは入力inputの名詞を調べ、辞書のテンプレートの%noun%をその名詞で置き換えて返す。"}
   response :template [{:keys [parts dictionary] :as params}]
