594a5c4d75aedaf35336650f73517ace289c95f6
sandmark <sandmark.m@gmail.com>
2019-11-16 01:59:44 +0900

    Modify project description

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject unmo "0.1.1"
-  :description "A japanese legacy chatbot using Sudachi, a morphological analyser for modern era."
+  :description "Unmo: A simple japanese chatbot."
   :url "https://github.com/sandmark/unmo-clojure/"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["Sonatype" "https://oss.sonatype.org/content/repositories/snapshots"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [bigml/sampling "3.2"]
@@ -13,6 +13,6 @@
             [lein-auto "0.1.3"]]
   :main ^:skip-aot unmo.core
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[alembic "0.3.2"]
-                                  [eftest "0.5.3"]]}
+  :profiles {:dev     {:dependencies [[alembic "0.3.2"]
+                                      [eftest "0.5.3"]]}
              :uberjar {:aot :all}})
