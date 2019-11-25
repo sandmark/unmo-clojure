1e7f64d17c4622323d4ad9f262bbf23c0c17d7e8
sandmark <sandmark.m@gmail.com>
2019-11-25 19:56:06 +0900

    Re-define `what-responder` as a function instead of multi method.

--- src/unmo/responder.clj
+++ src/unmo/responder.clj
@@ -65,11 +65,11 @@
                             :type :fatal})
       (assoc params :response (rand-nth random)))))
 
-(defmethod
-  ^{:doc "WhatResponderは入力 :input に対し、常に 'inputってなに？' と返す。"}
-  response :what [{:keys [input] :as params}]
-  (->> (str input "ってなに？")
-       (assoc params :response)))
+(defn response-what
+  "Returns a string with a question mark appended to the end of
+  the :input value of the given map."
+  [{:keys [input]}]
+  (str input "？"))
 
 (defmethod
   ^{:doc "Responderの指定がない、もしくは存在しないResponderを指定された場合、IllegalArgumentException例外を投げる。"}
